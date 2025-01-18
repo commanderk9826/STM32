af8ee4669df8819419efe82db7ef0fd3f63a9ca2
2CHAEHOON6 <commanderk9826>
2025-01-18 19:13:12 +0900

    24.01.18

---
+++ LED_PWM/Core/Inc/uart.h
@@ -0,0 +1,21 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 16, 2025
+ *      Author: ch.lee
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+//Prototype 선언
+void initUart(UART_HandleTypeDef *);//변수명 생략가능
+char getUart();
+char* getMessage();
+
+
+#endif /* INC_UART_H_ */
